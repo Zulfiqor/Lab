c2b63e4cce0bbd05bfbf1de8b7baa85c1500b528
Guest <guest-Kej0EG@zizu-Lenovo-B570.(none)>
2014-05-28 23:28:58 +0400

    fonction send_file added

--- lab03/server.c
+++ lab03/server.c
@@ -9,6 +9,32 @@
 #define QUEUE_SIZE 5
 
 
+
+
+void send_file(char* filename, int socket)
+{
+	int bytes_read;
+	int bytes_written;
+	char buffer[80];
+	FILE* file = fopen(filename, "rb");
+
+	// get file size
+	fseek(file, 0L, SEEK_END);
+	size_t size = ftell(file);
+	fseek(file, 0L, SEEK_SET);
+
+	// send file size
+	write(socket, &size, sizeof(size_t));
+
+
+	// send file
+	do { 
+	    bytes_read = fread(buffer, 1, sizeof(buffer), file);
+    	bytes_written = write(socket, buffer, bytes_read);
+	} while (bytes_read > 0);
+	fclose(file);
+}
+
 void *thread_func(void *sock)
 {
 	int asock = *((int*)sock);
