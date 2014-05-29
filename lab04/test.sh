835e00dd60ccc1188db9e233a9be56cca9867efa
zizu <zizu@zizu-Lenovo-B570.(none)>
2014-05-29 10:29:09 +0400

    test.sh modified

--- lab04/test.sh
+++ lab04/test.sh
@@ -1,5 +1,5 @@
-echo "$1" > /proc/calc/arg1
-echo "$3" > /proc/calc/arg2
-echo "$2" > /proc/calc/operation
+echo "1" > /proc/calc/arg1
+echo "2" > /proc/calc/arg2
+echo "+" > /proc/calc/operation
 
 cat /proc/calc/result
